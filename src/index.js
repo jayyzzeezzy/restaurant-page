2ddd092363da91631fe904715802e3732673445e
James Zhou <jam9es@gmail.com>
2023-12-13 14:07:05 -0800

    Write functions to create NavBar and handle clicks

--- src/index.js
+++ src/index.js
@@ -1,4 +1,42 @@
 import makeHomePage from './homepage'; 
 
+// make navbar function
+function makeNavBar() {
+    const nav = document.querySelector('#nav');
+    const home = document.createElement('button');
+    const menu = document.createElement('button');
+    const contact = document.createElement('button');
+
+    home.classList.add('navBtn');
+    menu.classList.add('navBtn');
+    contact.classList.add('navBtn');
+
+    home.textContent = 'Home';
+    menu.textContent = 'Menu';
+    contact.textContent = 'Contact';
+
+    nav.appendChild(home);
+    nav.appendChild(menu);
+    nav.appendChild(contact);
+
+    return nav;
+};
+
+function handleNavClicks() {
+    const navBtn = document.querySelectorAll('.navBtn');
+
+    navBtn.forEach((button) => {
+        button.addEventListener('click', () => {
+            loadPage();
+        })
+    });
+};
+
+function loadPage() {
+    console.log('loadPage executed successfully.');
+};
+
+makeNavBar();
+handleNavClicks();
 console.log('Executed successfully.');
 makeHomePage();
